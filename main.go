ebc6d8fb3b050e25377677f6a1506f258c2bc6b6
zyvaro <aminkafrinewgen@gmail.com>
2025-09-29 11:52:31 +0800

    read dictionary

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+const DICT_PATH = "dict"
+const JMDICT_FILENAME = "JMdict_e.xml"
 
 func main() {
-	fmt.Println("hello, world!");
+	data, err := readDictionary(JMDICT_FILENAME)
+	if err != nil {
+		// TODO: Handle error properly
+		panic(err)
+	}
+
+	fmt.Println(string(data))
+}
+
+func readDictionary(dictName string) ([]byte, error) {
+	filename := path.Join(DICT_PATH, dictName)
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return data, nil
 }
